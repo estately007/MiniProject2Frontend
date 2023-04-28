560debc845562a10f2f6f7e39f398f049783e6f7
AnujRuhela7 <ruhela7777@gmail.com>
2023-04-28 11:44:49 +0530

    Fixed bug

--- src/utils/verifyToken.js
+++ src/utils/verifyToken.js
@@ -3,21 +3,22 @@ import {toast} from "react-toastify";
 import {setUser} from "./userSlice";
 
 const verifyToken = (token, navigate, dispatch) => {
-    axios.post('/extractToken', {token})
-        .then((obj) => {
-            if(obj.data.error) {
-                toast.error("You need to login again");
-                localStorage.removeItem("isLoggedIn");
-                navigate('/');
-            } else {
-                dispatch(setUser(obj.data));
-            }
-        })
-        .catch((error) => {
-            return {
-                errorMsg: error
-            }
-        });
+    axios
+      .post("https://miniproject2backend.onrender.com/extractToken", { token })
+      .then((obj) => {
+        if (obj.data.error) {
+          toast.error("You need to login again");
+          localStorage.removeItem("isLoggedIn");
+          navigate("/");
+        } else {
+          dispatch(setUser(obj.data));
+        }
+      })
+      .catch((error) => {
+        return {
+          errorMsg: error,
+        };
+      });
 }
 
 export default verifyToken;
